51705a48d4dfd9a489d92fbc707e778900a54356
CodeLHC <lhcrawford97@gmail.com>
2023-05-02 13:44:42 +0100

    search by title updates to sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,21 +1,55 @@
-import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import qs from "qs";
-
-const buildQueryString = (operation, valueObj) => {
-  const { search } = useLocation();
-  const currentQueryParams = qs.parse(search, { ignoreQueryPrefix: true });
-  const newQueryParams = {
-    ...currentQueryParams,
-    [operation]: JSON.stringify(valueObj),
-  };
-  return qs.stringify(newQueryParams, { addQueryPrefix: true, encode: false });
-};
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 const SideBar = () => {
+  const [query, setQuery] = useState("");
+  const { search } = useLocation();
+
+  const buildQueryString = (operation, valueObj) => {
+    const currentQueryParams = qs.parse(search, { ignoreQueryPrefix: true });
+    const newQueryParams = {
+      ...currentQueryParams,
+      [operation]: JSON.stringify({
+        ...JSON.parse(currentQueryParams[operation] || "{}"),
+        ...valueObj,
+      }),
+    };
+    return qs.stringify(newQueryParams, {
+      addQueryPrefix: true,
+      encode: false,
+    });
+  };
+
+  const navigate = useNavigate();
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const newQueryString = buildQueryString("query", {
+      title: { $regex: query },
+    });
+    navigate(newQueryString);
+  };
+
   return (
     <div>
-      Filter by City
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Search..."
+        />
+        <button type="submit">
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            style={{ color: "#9667e0" }}
+          />
+        </button>
+      </form>
+      <h4>Filter by City</h4>
       <Link to={buildQueryString("query", { city: "Manchester" })}>
         Manchester
       </Link>
@@ -26,7 +60,7 @@ const SideBar = () => {
       <Link to={buildQueryString("query", { city: "Liverpool" })}>
         Liverpool
       </Link>
-      Sort by
+      <h4>Sort by</h4>
       <Link to={buildQueryString("sort", { price: +1 })}>Price ascending</Link>
       <Link to={buildQueryString("sort", { price: -1 })}>Price descending</Link>
     </div>
