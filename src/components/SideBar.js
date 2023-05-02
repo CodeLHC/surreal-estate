d6684d6403de7ee526ebf33af931377bfaab6919
CodeLHC <lhcrawford97@gmail.com>
2023-05-02 11:35:07 +0100

    added sort by price links with querystring updates

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,14 +1,34 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import qs from "qs";
+
+const buildQueryString = (operation, valueObj) => {
+  const { search } = useLocation();
+  const currentQueryParams = qs.parse(search, { ignoreQueryPrefix: true });
+  const newQueryParams = {
+    ...currentQueryParams,
+    [operation]: JSON.stringify(valueObj),
+  };
+  return qs.stringify(newQueryParams, { addQueryPrefix: true, encode: false });
+};
 
 const SideBar = () => {
   return (
     <div>
-      SideBar
-      <Link to={`/?query={"city": "Manchester"}`}>Manchester</Link>
-      <Link to={`/?query={"city": "Leeds"}`}>Leeds</Link>
-      <Link to={`/?query={"city": "Sheffield"}`}>Sheffield</Link>
-      <Link to={`/?query={"city": "Liverpool"}`}>Liverpool</Link>
+      Filter by City
+      <Link to={buildQueryString("query", { city: "Manchester" })}>
+        Manchester
+      </Link>
+      <Link to={buildQueryString("query", { city: "Leeds" })}>Leeds</Link>
+      <Link to={buildQueryString("query", { city: "Sheffield" })}>
+        Sheffield
+      </Link>
+      <Link to={buildQueryString("query", { city: "Liverpool" })}>
+        Liverpool
+      </Link>
+      Sort by
+      <Link to={buildQueryString("sort", { price: +1 })}>Price ascending</Link>
+      <Link to={buildQueryString("sort", { price: -1 })}>Price descending</Link>
     </div>
   );
 };
